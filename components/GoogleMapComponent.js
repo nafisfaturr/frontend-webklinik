54a6f4e9e33a22122241dd678fbf6b20482de772
IndraMaki <indramakiwiguna@gmail.com>
2024-11-14 19:43:17 +0700

    map vol3

--- components/GoogleMapComponent.js
+++ components/GoogleMapComponent.js
@@ -1,42 +1,16 @@
-'use client';
-import { Client } from '@vercel/postgres';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const GoogleMapComponent = () => {
-  useEffect(() => {
-    // Make sure the script is loaded before initializing the map
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBGM4Yg3SrswevROwh5wWlvi2isuXqeAeU&callback=initMap`;
-    script.async = true;
-    script.defer = true;
-    script.onload = () => {
-      if (typeof window !== 'undefined') {
-        // Initialize the map after the script has loaded
-        window.initMap = () => {
-          const map = new window.google.maps.Map(document.getElementById('map'), {
-            center: { lat: -6.591939198058985, lng: 106.80684396192702 },
-            zoom: 8,
-          });
-
-          // Add a marker at a specific location
-          const marker = new window.google.maps.Marker({
-            position: { lat: -6.591939198058985, lng: 106.80684396192702 },
-            map: map,
-            title: 'My Marker', // Optional title for the marker
-          });
-        };
-      }
-    };
-    document.head.appendChild(script);
-
-    // Clean up the script when the component is unmounted
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
   return (
-    <div id="map" style={{ height: '600px', width: '80%',  borderRadius: '15px' }}></div>
+    <iframe
+      width="80%"
+      height="600"
+      style={{ border: 0, borderRadius:15 }}
+      loading="lazy"
+      allowFullScreen
+      referrerPolicy="no-referrer-when-downgrade"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBGM4Yg3SrswevROwh5wWlvi2isuXqeAeU&q=-6.591831887585396, 106.80680873492207"
+    ></iframe>
   );
 };
 
